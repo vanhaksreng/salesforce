60cc476378c5d1105aea1c6160e22fbc35160211
YonBlue <yon@blue.com.kh>
2025-11-09 21:09:09 +0700

    Commited : 20251109_210909

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/macbook/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/macbook/projects/salesforce"
+export "FLUTTER_ROOT=/Users/annyon/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/annyon/projects/flutters/salesforce"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
